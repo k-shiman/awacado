3fc2db257b1deb123676e9b8e4d54e5abc4717c8
k-shiman <shiman.konstantin1@gmail.com>
2023-03-10 13:13:52 -0500

    fix echo.js

--- commands/echo.js
+++ commands/echo.js
@@ -9,15 +9,8 @@ module.exports = {
 		option.setName('input')
 			.setDescription('The input to echo back')),
 	async execute(interaction) {
-		await interaction.reply(setName);
+		const input = interaction.options.getUser('input');
+
+        await interaction.reply(input);
 	},
 };
-
-// const { SlashCommandBuilder } = require('discord.js');
-
-// const data = new SlashCommandBuilder()
-// 	.setName('echo')
-// 	.setDescription('Replies with your input!')
-// 	.addStringOption(option =>
-// 		option.setName('input')
-// 			.setDescription('The input to echo back'));
