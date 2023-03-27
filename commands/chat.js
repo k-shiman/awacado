d84e1379e09fb7dccb041b7f619f8552cdfb7cc9
k-shiman <shiman.konstantin1@gmail.com>
2023-03-26 23:48:31 -0400

    added comments

--- commands/chat.js
+++ commands/chat.js
@@ -1,8 +1,5 @@
-// const { SlashCommandBuilder } = require('discord.js');
-// Importing discord.js and destructuring SlashCommandBuilder from it
-
-const axios = require('axios');
-// Importing axios, which is a promise-based HTTP client for the browser and node.js
+const { SlashCommandBuilder } = require('discord.js'); // Importing discord.js and destructuring SlashCommandBuilder from it
+const axios = require('axios'); // Importing axios, which is a promise-based HTTP client for the browser and node.js
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,13 +8,9 @@ module.exports = {
         .addStringOption(option =>
             option.setName('input')
                 .setDescription('Enter your prompt here')
-                .setRequired(true)),
-    // module exports an object with 'data' property containing command info (name, description and options) and execute method
-   
+                .setRequired(true)), // module exports an object with 'data' property containing command info (name, description and options) and execute method
     async execute(interaction) {
-        const prompt = interaction.options.getString('input'); 
-        // get the user input/prompt as a string from options
-
+        const prompt = interaction.options.getString('input'); // get the user input/prompt as a string from options
         try {
             const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
                 prompt,
@@ -31,18 +24,12 @@ module.exports = {
                     'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
                     'Content-Type': 'application/json'
                 }
-            });
-            // Send POST request to the OpenAI API with the prompt and related parameters 
-
-            const messageContent = response.data.choices[0].text.trim();
-            // get the generated message from choices array of response data
-            
+            });// Send POST request to the OpenAI API with the prompt and related parameters 
+            const messageContent = response.data.choices[0].text.trim(); // get the generated message from choices array of response data
             await interaction.reply(messageContent);
-            // sending the generated text back as the response message to user's input.
         } catch (error) {
             console.error(error);
-            await interaction.reply('There was an error processing your request, please try again later.');
-            // if any errors encountered during the process, logging the error and sending error message
+            await interaction.reply('There was an error processing your request, please try again later.'); // if any errors encountered during the process, logging the error and sending error message
         }
     },
 };
